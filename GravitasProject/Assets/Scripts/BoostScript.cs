78eefb65d1af2d297e3f04966d32379b2748f3f4
RegsaGC <asgerhb@gmail.com>
2015-03-21 12:15:14 +0100

    Add visual feedback to BoostScript, Player should now have LineRenderer component

--- GravitasProject/Assets/Scripts/BoostScript.cs
+++ GravitasProject/Assets/Scripts/BoostScript.cs
@@ -8,28 +8,62 @@ public class BoostScript : MonoBehaviour {
 	public Component cameraObject;
 	public LayerMask compatibleLayer;
 
+	public float animationDelay = 0.1f;
+	public Color lineColourHit = Color.magenta;
+	public Color lineColourMiss = Color.magenta;
+
+	LineRenderer traceLine;
+
 	// Use this for initialization
 	void Start () {
 		//Debug.Log ("Look what i found! " + rigidbody2D.ToString ());
+
+		try{
+			traceLine = (LineRenderer) gameObject.GetComponent("LineRenderer");
+		}catch{
+			Debug.Log("There is no Line Renderer attatche to this object. There will be no visual feeback.");
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown("Fire1") ) 
 		{
-			Vector2 position = new Vector2 (transform.position.x, transform.position.y);
+			Vector2 currentPosition = new Vector2 (transform.position.x, transform.position.y);
 			Vector3 mousePosition =  cameraObject.GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);
-			Vector2 direction = new Vector2 (mousePosition.x, mousePosition.y) - position;
+			Vector2 direction = new Vector2 (mousePosition.x, mousePosition.y) - currentPosition;
 
-			RaycastHit2D rayHit = Physics2D.Raycast(position, direction, boostRange, compatibleLayer);
+			RaycastHit2D rayHit = Physics2D.Raycast(currentPosition, direction, boostRange, compatibleLayer);
+
+
 
 			if (rayHit) {
-				GetComponent<Rigidbody2D>().AddForce(Vector2.ClampMagnitude(direction, 1) * -boostPower);
+				GetComponent<Rigidbody2D>().AddForce(direction.normalized * -boostPower);
+				StartCoroutine(DrawBoostTrace(currentPosition, rayHit.point, lineColourHit));
 				//Debug.Log ("Boost recieved of size " + (Vector2.ClampMagnitude(direction, 1) * boostPower).magnitude.ToString() + " and value " + (Vector2.ClampMagnitude(direction, 1) * boostPower).ToString());
+
+			}else{
+				StartCoroutine(DrawBoostTrace(currentPosition, currentPosition + direction.normalized * boostRange, lineColourMiss));
 			}
 
 			//Debug.Log ("Mouse is at " + mousePosition.ToString() + " and object is at " + position.ToString() + ".");
 			//Debug.Log("Resulting direction: " + direction.ToString()+ "  And the ray hit " + rayHit.transform);
 		}
+	}
+
+
+	IEnumerator DrawBoostTrace(Vector2 origin, Vector2 destination, Color lineColour){
+		Color lineColourStart = new Color(lineColour.r, lineColour.g, lineColour.b, 0);
+		Color lineColourEnd = lineColour;
+		traceLine.SetColors(lineColourStart, lineColourEnd);
+		traceLine.SetVertexCount(2);
+		traceLine.SetPosition(0, origin);
+		traceLine.SetPosition(1, destination);
+
+		yield return new WaitForSeconds (animationDelay);
+
+		traceLine.SetVertexCount(0);
+
+		 yield break;
 	}
 }
--- README.md
+++ README.md
@@ -11,5 +11,18 @@ If you somehow mannage to make more than $5000 from this project, we would like 
 Todo:
 * Add cooldown to BoostScript preventing the player from spamming the button for greater acceleration
 * Add graphical feedback to BoostScript
+* Add development-tool: image to level converter.
+* Add development-tool: see if you can't find a neat way to add coordinates to the enemy routes
 * All of the soundtrack
-* The levels, of cause
+# The levels, of cause
+ * Add a couple of enemy types
+ # A couple of demo-levels of diferent kinds
+  * One like a linear obstacle-course
+  * One open-ended with very small rooms
+  * One open-ended with huge open spaces
+  * One open-ended with each room an obstacle-course
+# A couple of texture todos:
+ * One for the hurtful block
+ * One for the checkpoint(I am still not happy with it)
+ * More enemy-textures, preferably
+ * At least one teleporter-texture
