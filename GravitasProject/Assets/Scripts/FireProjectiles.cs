7607ec2a7487ef62b4925758cccdf93ce3c462b3
RegsaGC <asgerhb@gmail.com>
2015-05-05 13:10:23 +0200

    Add jester enemy who fires projectiles at interval

---
+++ GravitasProject/Assets/Scripts/FireProjectiles.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FireProjectiles : MonoBehaviour
+{
+
+	[Tooltip ("The prefab to use as projectile")]
+	public GameObject
+		projectile;
+	[Tooltip("In seconds. Leave at 0 to disable")]
+	public float
+		fireInterval;
+	[Tooltip("Initial speed in the direction it is fired. Needs a Rigidbody")]
+	public float
+		projectileSpeed = 1;
+	[Tooltip ("Angles in degrees. Will fire every direction every time.")]
+	public float[]
+		directionsToFire;
+	
+
+
+	void Start ()
+	{
+		StartCoroutine ("fireContinually");
+	}
+
+	void Update ()
+	{
+	
+	}
+
+	IEnumerator fireContinually ()
+	{
+		Vector3 currentPosition;
+
+		while (fireInterval > 0) {
+			yield return new WaitForSeconds (fireInterval);
+
+			currentPosition = transform.position;
+
+			for (int i = directionsToFire.Length-1; i> -1; i--) {
+
+				GameObject fired = Instantiate (projectile);
+				fired.transform.position = currentPosition;
+				fired.transform.eulerAngles = new Vector3 (0, 0, directionsToFire [i]);
+
+				if (fired.GetComponent<Rigidbody2D> ()) {
+					Vector2 dirVec = new Vector2 
+						(Mathf.Cos (Mathf.Deg2Rad * directionsToFire [i]), 
+						 Mathf.Sin (Mathf.Deg2Rad * directionsToFire [i]));
+					fired.GetComponent<Rigidbody2D> ().velocity = dirVec * projectileSpeed;
+				}
+			}
+		}
+	}
+}
---
+++ GravitasProject/Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Place on an object to have it move forwards(local space) unhindered by any gravity and gravitrons.
+ * Will optionally self-destruct when hitting colliders.
+*/
+using UnityEngine;
+using System.Collections;
+
+public class ProjectileScript : MonoBehaviour
+{
+
+	[Tooltip("Time in seconds before the object is destroyed automatically")]
+	public float
+		lifetime = 100;
+	[Tooltip("Wheter the object is destroyed when touching a collider")]
+	public bool
+		destroyOnCollision = true;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Destroy (gameObject, lifetime);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		Debug.Log ("I hit something!");
+		if (destroyOnCollision && other.isTrigger == false) {
+			Destroy (gameObject, Time.fixedDeltaTime);
+		}
+	}
+}
