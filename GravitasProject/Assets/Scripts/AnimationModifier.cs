8efea9c60017d75310c18ba3c0a8373d9a3132cd
RegsaGC <asgerhb@gmail.com>
2015-08-22 17:44:59 +0200

    Add new graphics(blocks). Do major cleanup of /Resource/

---
+++ GravitasProject/Assets/Scripts/AnimationModifier.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationModifier : MonoBehaviour
+{
+
+	[Tooltip("1 is normal animation playback.")]
+	public float
+		speed;
+
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<Animator> ().speed = speed;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
